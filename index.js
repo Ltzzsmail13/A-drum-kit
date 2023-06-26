3b12e73e8c38911c0540c2f35edf8a909e8cfddc
Ltzzsmail13 <124031987+Ltzzsmail13@users.noreply.github.com>
2023-06-26 15:01:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const btn = document.querySelectorAll("button");
 console.log(btn)
 
-const w = new Audio("/sounds/tom-1.mp3");
-const a = new Audio("/sounds/tom-2.mp3");
-const s = new Audio("/sounds/tom-3.mp3");
-const d = new Audio("/sounds/tom-4.mp3");
-const j = new Audio("/sounds/snare.mp3");
-const k = new Audio("/sounds/crash.mp3");
-const l = new Audio("/sounds/kick-bass.mp3")
 
 btn.forEach( button => button.addEventListener("click", function(){
     const btnInnerHtml = this.innerHTML;
@@ -25,24 +18,37 @@ const makeSound = (key) => {
 
     switch (key) {
         case "w":
+            const w = new Audio("/sounds/tom-1.mp3");
             w.play();
             break;
+            
         case "a":
+            const a = new Audio("/sounds/tom-2.mp3");
             a.play();
             break;
+            
         case "s":
+            const s = new Audio("/sounds/tom-3.mp3");  
             s.play();
             break;
+            
         case "d":
+            const d = new Audio("/sounds/tom-4.mp3");
             d.play();
             break;
+            
         case "j":
+            const j = new Audio("/sounds/snare.mp3");
             j.play();
             break;
+            
         case "k":
+            const k = new Audio("/sounds/crash.mp3");
             k.play();
             break;
+            
         case "l":
+            const l = new Audio("/sounds/kick-bass.mp3")
             l.play();
             break;        
         default:
